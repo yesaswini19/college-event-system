3eeb06a0266bb3835fe87c8209a984bcd848a6a5
yesaswini19 <yesaswinipamidi19@gmail.com>
2026-01-31 19:01:35 +0530

    Changed db.js

--- college-event-system/backend/db.js
+++ college-event-system/backend/db.js
@@ -1,14 +1,15 @@
-const mysql = require("mysql2"); // Removed /promise
+const mysql = require("mysql2");
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: { rejectUnauthorized: false } // Add this for cloud databases!
 });
 
 module.exports = db;
